16c31f22f898533ef631d160716a38f26c2c7406
namtran912 <trantunam.cqtk8@gmail.com>
2016-08-23 16:30:01 +0700

    implement

--- test/src/main/scala/test/JettyService.scala
+++ test/src/main/scala/test/JettyService.scala
@@ -1,6 +1,7 @@
 package test
 
 import javax.servlet.ServletConfig
+import javax.servlet.annotation.WebServlet
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 import org.eclipse.jetty.http.HttpStatus
@@ -8,6 +9,9 @@ import org.eclipse.jetty.http.HttpStatus
 /**
   * Created by CPU11179-local on 7/19/2016.
   */
+
+
+)
 class JettyService extends HttpServlet{
 
   override def init(config: ServletConfig): Unit = {
@@ -16,7 +20,6 @@ class JettyService extends HttpServlet{
 
   override def doGet(request: HttpServletRequest , response: HttpServletResponse ): Unit = {
       response.setStatus(HttpStatus.OK_200)
-
       val ip = request.getParameter("ip")
       if (ip != null)
           response.getWriter().println(Process.getLocationCode(ip))
